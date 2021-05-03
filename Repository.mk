cf5c9b9f7fc631b55da822a04dfef0cc4abadcb6
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-05-03 16:42:22 +0900

    Add PDF importer to read a PDF into drawinglayer primitives
    Change-Id: I8d2e28a39515dfef8a1b4c6b06df095dd3a4eaec

--- Repository.mk
+++ Repository.mk
@@ -448,6 +448,7 @@ $(eval $(call gb_Helper_register_libraries_for_install,OOOLIBS,ooo, \
 	passwordcontainer \
 	pcr \
 	pdffilter \
+	pdfimporter \
 	$(call gb_Helper_optional,SCRIPTING,protocolhandler) \
 	sax \
 	sb \
