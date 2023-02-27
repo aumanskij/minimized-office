wwjiang007/LibreOffice-core

WASM default to notebookbar
..and use the full, desktop variant..

Change-Id: Ib00aad8cd130b4a3433209c540fe82970c45c98e

desktop/source/lib/init.cxx
@@ -6397,7 +6397,7 @@ static void activateNotebookbar(std::u16string_view rApp)
 
     if (aAppNode.isValid())
     {
-        aAppNode.setNodeValue("Active", makeAny(OUString("notebookbar_online.ui")));
+        aAppNode.setNodeValue("Active", makeAny(OUString("notebookbar.ui")));
         aAppNode.commit();
     }
 }
@@ -6416,7 +6416,7 @@ static int lo_initialize(LibreOfficeKit* pThis, const char* pAppPath, const char
     static bool bPreInited = false;
     static bool bUnipoll = false;
     static bool bProfileZones = false;
-    static bool bNotebookbar = false;
+    static bool bNotebookbar = true;
 
     { // cf. string lifetime for preinit
         std::vector<OUString> aOpts;

aumanskij/minimized-office

desktop/source/lib/init.cxx
@@ -7323,7 +7323,11 @@ static void activateNotebookbar(std::u16string_view rApp)
 
     if (aAppNode.isValid())
     {
+#ifdef EMSCRIPTEN
+        aAppNode.setNodeValue("Active", Any(OUString("notebookbar.ui")));
+#else
         aAppNode.setNodeValue("Active", Any(OUString("notebookbar_online.ui")));
+#endif
         aAppNode.commit();
     }
 }
@@ -7446,7 +7450,11 @@ static int lo_initialize(LibreOfficeKit* pThis, const char* pAppPath, const char
     static bool bPreInited = false;
     static bool bUnipoll = false;
     static bool bProfileZones = false;
+#ifdef EMSCRIPTEN
+    static bool bNotebookbar = true;
+#else
     static bool bNotebookbar = false;
+#endif
 
     { // cf. string lifetime for preinit
         std::vector<OUString> aOpts;